40c782d2f1b01e054051ba2bba297b8f73db970b
Abdel majid Kansoussi <kansoussi9763@gmail.com>
2023-05-29 13:25:44 +0100

    adding authors routes

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express");
-const app = express();
 const expressLayouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -22,7 +25,9 @@ app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", useNewUrlParser: true }));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
